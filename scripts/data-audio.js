09ae0b0d92580d3af470cd09e771bc8933f8e702
Elias Menezes Júnior <eliasmenezes2510@gmail.com>
2021-10-18 01:25:42 -0300

    mySound

---
+++ scripts/data-audio.js
@@ -0,0 +1,182 @@
+window.sounds = [
+
+    // ROCK
+    [
+        {
+            title: "Emergency On Level 3",
+            artist: "Jeremy Korpas",
+            cover: "./assets/thumbnail/rock-card-1.jpg",
+            file: "https://cdn.discordapp.com/attachments/897951441094840350/898701294200586290/Emergency_On_Level_3_Jeremy_Korpas.mp3",
+        },
+
+        {
+            title: "Sharp",
+            artist: "Jeremy Korpas",
+            cover: "./assets/thumbnail/rock-card-2.jpg",
+            file: "https://cdn.discordapp.com/attachments/897951441094840350/898701704927801354/Sharp_-_Jeremy_Korpas.mp3",
+        },
+
+        {
+            title: "Ten Inch Spikes",
+            artist: "Jeremy Korpas",
+            cover: "./assets/thumbnail/rock-card-3.jpg",
+            file: "https://cdn.discordapp.com/attachments/897951441094840350/898701907978244166/Ten_Inch_Spikes_-_Jeremy_Korpas.mp3",
+        },
+
+        {
+            title: "The Beacon",
+            artist: "Zachariah Hickman",
+            cover: "./assets/thumbnail/rock-card-4.jpg",
+            file: "https://cdn.discordapp.com/attachments/897951441094840350/898702228909596752/The_Beacon_-_Zachariah_Hickman.mp3",
+        },
+
+        {
+            title: "Twin Engines",
+            artist: "Jeremy Korpas",
+            cover: "./assets/thumbnail/rock-card-5.jpg",
+            file: "https://cdn.discordapp.com/attachments/897951441094840350/898702642228908092/Twin_Engines_-_Jeremy_Korpas.mp3",
+        },
+
+        {
+            title: "Tropical Thunder",
+            artist: "RKVC",
+            cover: "./assets/thumbnail/rock-card-5.jpg",
+            file: "https://cdn.discordapp.com/attachments/897951441094840350/899009317108322324/Tropical_Thunder_-_RKVC.mp3",
+        },
+    ],
+
+    // HIP_HOP
+    [
+        {
+            title: "Bay Street Billionaires",
+            artist: "Squadda B",
+            cover: "./assets/thumbnail/hip-hop-card-1.jpg",
+            file: "https://cdn.discordapp.com/attachments/897951441094840350/899044855504838716/Bay_Street_Billionaires_-_Squadda_B.mp3",
+        },
+
+        {
+            title: "North Oakland Extasy",
+            artist: "Squadda B",
+            cover: "./assets/thumbnail/hip-hop-card-2.jpg",
+            file: "https://cdn.discordapp.com/attachments/897951441094840350/899045396960149534/North_Oakland_Extasy_-_Squadda_B.mp3",
+        },
+
+        {
+            title: "OSA Dress Code",
+            artist: "Squadda B",
+            cover: "./assets/thumbnail/hip-hop-card-3.jpg",
+            file: "https://cdn.discordapp.com/attachments/897951441094840350/899046150303272990/OSA_Dress_Code_-_Squadda_B.mp3",
+        },
+
+        {
+            title: "Space Age Hustle",
+            artist: "Squadda B",
+            cover: "./assets/thumbnail/rock-card-4.jpg",
+            file: "https://cdn.discordapp.com/attachments/897951441094840350/899046401898598440/Space_Age_Hustle_-_Squadda_B.mp3",
+        },
+
+        {
+            title: "Today's Plan",
+            artist: "DJ Freedem",
+            cover: "./assets/thumbnail/rock-card-5.jpg",
+            file: "https://cdn.discordapp.com/attachments/897951441094840350/899046551987572797/Todays_Plan_-_DJ_Freedem.mp3",
+        },
+
+        {
+            title: "Forever",
+            artist: "Forever - Anno Domini Beats",
+            cover: "./assets/thumbnail/rock-card-5.jpg",
+            file: "https://cdn.discordapp.com/attachments/897951441094840350/899051336119771136/Forever_-_Anno_Domini_Beats.mp3",
+        },
+    ],
+    
+    // DANCE
+    [
+        {
+            title: "Nine Lives",
+            artist: "Unicorn Heads",
+            cover: "./assets/thumbnail/rock-card-1.jpg",
+            file: "https://cdn.discordapp.com/attachments/897951441094840350/899075382106554368/Bonfire_-_An_Jone.mp3",
+        },
+
+        {
+            title: "Der Königk",
+            artist: "Jeremy Black",
+            cover: "./assets/thumbnail/rock-card-2.jpg",
+            file: "https://cdn.discordapp.com/attachments/897951441094840350/899075413282803772/Der_Konig_-_Jeremy_Black.mp3",
+        },
+
+        {
+            title: "First Of 3",
+            artist: "Jeremy Black",
+            cover: "./assets/thumbnail/rock-card-3.jpg",
+            file: "https://cdn.discordapp.com/attachments/897951441094840350/899075482652385280/First_Of_3_-_Jeremy_Black.mp3",
+        },
+
+        {
+            title: "Hannon",
+            artist: "Jeremy Black",
+            cover: "./assets/thumbnail/rock-card-4.jpg",
+            file: "https://cdn.discordapp.com/attachments/897951441094840350/899075482652385280/First_Of_3_-_Jeremy_Black.mp3",
+        },
+
+        {
+            title: "Nine Lives",
+            artist: "Unicorn Heads",
+            cover: "./assets/thumbnail/rock-card-5.jpg",
+            file: "https://cdn.discordapp.com/attachments/897951441094840350/899075583714156564/Nine_Lives_Unicorn_Heads1.mp3",
+        },
+
+        {
+            title: "Calvin Harris",
+            artist: "Josh Pan",
+            cover: "./assets/thumbnail/rock-card-5.jpg",
+            file: "https://cdn.discordapp.com/attachments/897951441094840350/899079057415766067/Calvin_Harris_-_josh_pan.mp3",
+        },
+    ],
+
+    // COUNTRY
+    [
+        {
+            title: "Leaning On the Everlasting Arms",
+            artist: "Zachariah Hickman",
+            cover: "./assets/thumbnail/rock-card-1.jpg",
+            file: "https://cdn.discordapp.com/attachments/897951441094840350/899082493314093066/Leaning_On_the_Everlasting_Arms_-_Zachariah_Hickman.mp3",
+        },
+
+        {
+            title: "Mourning Dove",
+            artist: "Zachariah Hickman",
+            cover: "./assets/thumbnail/rock-card-1.jpg",
+            file: "https://cdn.discordapp.com/attachments/897951441094840350/899082522560983050/Mourning_Dove_-_Zachariah_Hickman.mp3",
+        },
+
+        {
+            title: "In the Sweet By and By",
+            artist: "Zachariah Hickman",
+            cover: "./assets/thumbnail/rock-card-1.jpg",
+            file: "https://cdn.discordapp.com/attachments/897951441094840350/899082556685840424/In_the_Sweet_By_and_By_-_Zachariah_Hickman.mp3",
+        },
+
+        {
+            title: "The Colonel",
+            artist: "Zachariah Hickman",
+            cover: "./assets/thumbnail/rock-card-1.jpg",
+            file: "https://cdn.discordapp.com/attachments/897951441094840350/899082788530159636/The_Colonel_-_Zachariah_Hickman.mp3",
+        },
+        
+        {
+            title: "Ratatouille's Kitchen",
+            artist: "Carmen María and Edu Espinal",
+            cover: "./assets/thumbnail/rock-card-1.jpg",
+            file: "https://cdn.discordapp.com/attachments/897951441094840350/899082832922677278/Ratatouilles_Kitchen_-_Carmen_Maria_and_Edu_Espinal.mp3",
+        },
+
+        {
+            title: "The Slow Rabbit",
+            artist: "Nat Keefe & Hot Buttered Rum",
+            cover: "./assets/thumbnail/rock-card-1.jpg",
+            file: "https://cdn.discordapp.com/attachments/897951441094840350/899082882662957066/The_Slow_Rabbit_-_Nat_Keefe__Hot_Buttered_Rum.mp3",
+        },
+    ],
+]
